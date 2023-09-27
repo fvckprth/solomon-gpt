9af5230d8f3721fcddf69d7c534f52784820166b
princeofdev <lucky.charge.x@gmail.com>
2023-09-27 13:50:12 -0500

    Store Chat history in Supabase

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -3,18 +3,22 @@
 import { useChat, Message } from 'ai/react'
 import { useEffect } from 'react'
 import { cn } from '@/lib/utils'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { ChatList } from '@/components/chat/main/ChatList'
 import { ChatPanel } from '@/components/chat/main/ChatPanel'
 import Examples from '@/components/chat/main/Examples'
 import { ChatScrollAnchor } from '@/components/chat/main/ChatScrollAnchor'
 import WelcomeBox from './main/WelcomeBox'
+import { addChat } from '@/app/actions'
+import { CreateMessage } from 'ai'
 
 export interface ChatProps extends React.ComponentProps<'div'> {
   initialMessages?: Message[]
-  id?: string
+  id?: string,
+  userId?: string
 }
 
-export function Chat({ id, initialMessages, className }: ChatProps) {
+export function Chat({ id, initialMessages, className, userId }: ChatProps) {
     const { messages, append, reload, stop, isLoading, input, setInput } =
       useChat({
         initialMessages,
@@ -26,8 +30,18 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
           if (response.status === 401) {
             throw new Error(response.statusText)
           }
+        },
+        onFinish(message: Message) {
+          console.log('finished message ', message)
+          addToChatHistory(message);
         }
-      })
+    })
+
+
+    const addToChatHistory = async (message : Message | CreateMessage) => {
+      const response = await addChat(userId, message.role, message.content)
+      console.log('adding new message: ', response);
+    }
 
     useEffect(() => {
         if (messages.length === 0) {
@@ -37,6 +51,8 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
           document.body.style.overflow = 'auto';
         }
       }, [messages.length]);
+    
+    console.log('messages:  ', messages)
 
     return (
     <div>
@@ -62,6 +78,7 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
           messages={messages}
           input={input}
           setInput={setInput}
+          addToChatHistory={addToChatHistory}
         />
       </div>
     )
