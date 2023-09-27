976152539b9ed2a46ffe3221c3f93f9c1afa5f6b
fvckprth <parth@eastpark.xyz>
2023-09-26 21:41:42 -0400

    Header tweak

--- components/chat/nav/Header.tsx
+++ components/chat/nav/Header.tsx
@@ -7,7 +7,7 @@ export async function Header() {
   const cookieStore = cookies()
   const session = await auth({ cookieStore })
   return (
-    <div className="sticky top-0 z-50 md:mx-auto mx-4 flex h-16 items-center justify-between border-b px-4">
+    <div className="sticky top-0 z-50 md:mx-auto mx-4 flex h-16 items-center justify-between px-4">
       <div className="flex items-center">
         <UserMenu session={session} />      
       </div>
